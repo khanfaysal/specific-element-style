337ae1da9831c14cbe6b473abf164bb149100cfb
khanfaysal <mdfaysalkhancse@gmail.com>
2021-10-10 18:11:30 +0600

    latest code

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyASZRhs4qMvvj3a5FOko72lRxHECPcrOzI",
+    authDomain: "fir-crud-rt.firebaseapp.com",
+    projectId: "fir-crud-rt",
+    storageBucket: "fir-crud-rt.appspot.com",
+    messagingSenderId: "353712866904",
+    appId: "1:353712866904:web:79a3cd963bb4a4b7016c2d",
+    measurementId: "G-TKMS856VQY",
+    databaseURL: "https://fir-crud-rt-default-rtdb.firebaseio.com/",
+};
+export default firebaseConfig;
