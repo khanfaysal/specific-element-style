d6fac777f83bf8e6c54c384f56926463006dad3a
lincoln khan <mdfaysalkhancse@gmail.com>
2021-11-14 02:22:52 +0600

    react hook form validation with yup done primarily

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, { Component } from "react";
-import "tailwindcss/tailwind.css";
 import Clock from "./components/Clock";
-import { userSchema } from "../src/Validations/UserValidations";
-import * as yup from "yup";
+import Form from "./components/Form";
+
 class App extends Component {
   render() {
-    const createUser = (e) => {
-      e.preventDefault();
-    };
     return (
       <div>
         <Clock locale="bn-BD" />
-        <form onSubmit={createUser}>
-          <input type="text" placeholder="Name"></input>
-          <input type="email" placeholder="Email"></input>
-          <input type="password" placeholder="Password"></input>
-        </form>
+        <Form />
       </div>
     );
   }
